c43fc40552490b71c43999d7f81977b4a2c85ec2
Aquib <aquibshaikh02397@gmail.com>
2020-12-09 15:02:15 +0530

    added changes

--- src/app.js
+++ src/app.js
@@ -12,7 +12,65 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // your code goes here
+app.get("/mario", async (req,res) =>{
+    res.send(await marioModel.find({}));
+})
 
+app.get("/mario/:id", async (req, res) => {
+  try {
+    let result = await marioModel.findOne({ _id: req.params.id });
+    if (result === null) {
+      throw Error;
+    } else {
+      res.send(result);
+    }
+  } catch (er) {
+    res.status(400).send({ message: er.message });
+  }
+});
+const isNoU = (i) => i===null || i===undefined
+app.post("/mario",async (req,res)=>{
+    let {name, weight} = req.body;
+    
+    if (!(isNoU(name)  || isNoU(weight))){
+        let newObj = {name,weight};
+        let newMario = new marioModel(newObj);
+        await newMario.save(); 
+    
+        res.status(201).send(newMario);
 
-
+    }else{
+        res.status(400).send({message:'either name or weight is missing'})   
+    }
+})
+app.patch("/mario/:id", async (req, res) => {
+    try {
+      let {name,weight} = req.body;
+      let result = await marioModel.findOne({ _id: req.params.id });
+      console.log(result);
+      if (result === null || (isNoU(name) && isNoU(weight))) {
+        res.status(400).send({message:"id not found"});
+      } else {
+        result.weight = weight ?? result.weight;
+        result.name = name ?? result.name;
+        await result.save();
+        res.send(result);
+      }
+    } catch (er) {
+      res.status(400).send({ message: er.message });
+    }
+  });
+app.delete("/mario/:id", async (req,res) => {
+    try{
+        let d = await marioModel.findOne({_id:req.params.id});
+        await marioModel.deleteOne({_id:req.params.id});
+        res.send({
+            message:"character deleted",
+        })
+    } catch(er){
+        res.status(400).send({
+            message:er.message,
+        })
+    }
+})
 module.exports = app;
--- src/models/marioChar.js
+++ src/models/marioChar.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 //  Your code goes here
+const marioModel = mongoose.model("marioChar",new mongoose.Schema({
+    name:String,
+    weight:Number,
+}))
 
 
 module.exports = marioModel;
